b0556c593f2905b9e7e0c5df4908edb9dbe92cf4
Kyle Hollands <kylehollands@gmail.com>
2022-12-25 00:21:02 -0500

    Updated loop_sheets.js

--- loop_sheets.js
+++ loop_sheets.js
@@ -1,57 +1,26 @@
-function LOOP_THROUGH_SHEETS(rangeOne,rangeTwo,countOne,countTwo) {
+function LOOP_THROUGH_SHEETS_FOUR(rangeOne, countOne, countTwo) {
 
-    var count = 0
-    var cOne = 0
-    var cTwo = 0
-  
-    // Sheets to exclude (array)
-    var exclude = ["Meeting Volume","Moderators"];
-  
-    var ss = SpreadsheetApp.getActive(); // Acquire active sheet
-    var allsheets = ss.getSheets();
-  
-    for(var s in allsheets){
-      var sheet = allsheets[s];
-  
-      // Stop iteration execution if the condition is met.
-      if(exclude.indexOf(sheet.getName()) == 1) continue;
-        
-        sheet.getRange(rangeOne)
-          .getValues()
-          .reduce(function (a, b) {
-            rOne = a.concat(b)
-            return rOne;
-          })
-  
-        sheet.getRange(rangeTwo)
-          .getValues()
-          .reduce(function (a, b) {
-            rTwo = a.concat(b)
-            return rTwo;
-          })
-  
-        rOne.forEach(function (v) {
-          if (v === countOne) {
-            cOne += 1;
-          }
-        })
-        rTwo.forEach(function (x) {
-          if (x === countTwo) {  
-            cTwo += 1;
-          } 
-        })
-          //count += 1;  
-            
-          
-    } // End of loop
-    return cOne + cTwo;
-  
-  } // End of function
-  
-  
-  
-  
-  
-  
-  
-  
+  var count = 0
+
+  var ss = SpreadsheetApp.getActive();
+  var allsheets = ss.getSheets();
+
+  var exclude = ["Meeting Volume","Moderators"];
+
+  for(var s in allsheets){
+    var sheet = allsheets[s];
+    var range = sheet.getRange(rangeOne)
+    var values = range.getValues()
+
+    // Stop iteration execution if the condition is met.
+    //-------------------------------------------------
+    if(exclude.indexOf(sheet.getName()) == 1) continue;
+    //-------------------------------------------------
+      values.forEach(function(row) {
+        row.forEach(function(col) {
+          if (col === countOne && row[0,5] === countTwo) {count +=1}
+        });
+      });
+ // End of loop
+  } return count;
+}
